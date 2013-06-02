51bf31c1ad39397b5e273b1b5f52e071cf097534
Park Hyunwoo <ez@smartstudy.co.kr>
2013-06-02 13:54:01 +0900

    Add sample page

---
+++ shy-navbar.js
@@ -0,0 +1,48 @@
+//
+// shy-navbar.js - Add “shyness” into your navigation bar.
+// https://github.com/lqez/shy-navbar
+//
+// usage : addShyness("#navbar");
+//
+function addShyness(target)
+{
+    return function(target) {
+        // Change these variables to suit your favor.
+        var thresholdUp = 10;
+        var thresholdDown = 80;
+        var animationDelay = 250;
+        var animationDur = 150;
+
+        var $navbar = $(target);
+        $navbar.css({
+            'position': 'absolute',
+                 'top': '0',
+             'z-index': '1'
+        });
+        var lastScrollTop = 0;
+
+        $(window).scroll(function () { 
+            var position = $navbar.css('position');
+            var height = $navbar.height();
+            var scrollTop = $(this).scrollTop()
+
+            if( scrollTop < height ) {
+                $navbar.clearQueue().css({ top: 0, position: 'absolute' });
+            } else {
+                if( scrollTop < lastScrollTop ) {
+                    if( position != 'fixed' && ( scrollTop + thresholdDown < lastScrollTop ) ) {
+                        $navbar.clearQueue()
+                            .css({ top: '-'+height+'px', position: 'fixed' })
+                            .delay(animationDelay).animate({ top: 0 }, animationDur);
+                    }
+                } else if( position == 'fixed' && ( scrollTop - thresholdUp > lastScrollTop ) ) {
+                    $navbar.clearQueue()
+                        .delay(animationDelay).animate({ top: '-'+height+'px' }, animationDur, function() { 
+                            $(this).clearQueue().css({ top: 0, position: 'absolute' });
+                        });
+                }
+            }
+            lastScrollTop = scrollTop;
+        });
+    }(target);
+}
